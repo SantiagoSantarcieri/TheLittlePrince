855e3c3da312df22c1b2a725ae4a67d24e748e67
santi <santiagosantarcieri@gmail.com>
2022-06-09 22:33:16 -0300

    Version sin desarrollar ni botones ni publi

---
+++ app/src/main/java/com/example/thelittleprince/DedicationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.thelittleprince
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DedicationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dedication)
+    }
+}
---
+++ app/src/main/java/com/example/thelittleprince/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.thelittleprince
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(2000)
+        setTheme(R.style.Theme_TheLittlePrince)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val buttonLecture = findViewById<Button>(R.id.btnOpenActivityLecture)
+        buttonLecture.setOnClickListener {
+            val intent = Intent(this, LectureActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonAuthor = findViewById<Button>(R.id.btnOpenActivityAuthor)
+        buttonAuthor.setOnClickListener {
+            val intent = Intent(this, AuthorActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonDedication = findViewById<Button>(R.id.btnOpenActivityDedication)
+        buttonDedication.setOnClickListener {
+            val intent = Intent(this, DedicationActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+
+}
+
+
